ad89f8819b8e1fd8f0a0415bbdfe34c266dbe74a
Guilherme <guilherme.julio@d20labs.tech>
2022-09-03 17:59:45 -0300

    Translate initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,5 +39,39 @@ module.exports = {
         path: `${__dirname}/content/`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/locales`,
+        name: `locale`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-react-i18next`,
+      options: {
+        localeJsonSourceName: `locale`,
+        languages: [`en`, `pt`],
+        defaultLanguage: `pt`,
+        siteUrl: `http://localhost:8000/`,
+        i18nextOptions: {
+          interpolation: {
+            escapeValue: false,
+          },
+          keySeparator: false,
+          nsSeparator: false,
+        },
+        pages: [
+          {
+            matchPath: '/:lang?/blog/:uid',
+            getLanguageFromPath: true,
+            excludeLanguages: ['es'],
+          },
+          {
+            matchPath: '/preview',
+            languages: ['en'],
+          },
+        ],
+      },
+    },
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { graphql } from 'gatsby';
 import App from '../components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/main.scss';
 import Global from '../constants/Global';
 
-export default () => {
+// eslint-disable-next-line react/prop-types
+export default ({ data }) => {
+  console.log(data);
   const headData = Global.getHeadData;
   const { title, lang, description } = headData;
 
@@ -21,3 +24,17 @@ export default () => {
     </>
   );
 };
+
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
