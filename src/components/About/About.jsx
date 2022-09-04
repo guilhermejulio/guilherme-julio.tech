5c7ce9fc966370c96ca82c6db03f2171b531c07c
Guilherme <guilherme.julio@d20labs.tech>
2022-09-04 19:20:30 -0300

    About translation

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,24 +2,43 @@ import React, { useState, useEffect } from 'react';
 import Fade from 'react-reveal/Fade';
 import { Container, Row, Col } from 'react-bootstrap';
 import { graphql, useStaticQuery } from 'gatsby';
+import { useTranslation, useI18next } from 'gatsby-plugin-react-i18next';
 import Title from '../Title/Title';
 import AboutImg from '../Image/AboutImg';
 
 const About = () => {
+  const { t } = useTranslation();
+  const { language } = useI18next();
+
   const data = useStaticQuery(graphql`
     query {
-      pessoa: sobreYaml {
+      sobre: sobreYaml {
         img
         paragraphOne
         paragraphTwo
         paragraphThree
         resume
-        titleText
-        btnText
+      }
+      about: aboutYaml {
+        img
+        paragraphOne
+        paragraphTwo
+        paragraphThree
+        resume
+      }
+      acerca: acercaYaml {
+        img
+        paragraphOne
+        paragraphTwo
+        paragraphThree
+        resume
       }
     }
   `);
-  const about = data.pessoa;
+  let getAbout = data.sobre;
+  if (language === 'en') getAbout = data.about;
+  else if (language === 'es') getAbout = data.acerca;
+  const about = getAbout;
 
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -37,7 +56,7 @@ const About = () => {
   return (
     <section id="about">
       <Container>
-        <Title title={about?.titleText} />
+        <Title title={t('Sobre mim')} />
         <Row className="about-wrapper">
           <Col md={6} sm={12}>
             <Fade bottom duration={1000} delay={600} distance="30px">
@@ -50,16 +69,13 @@ const About = () => {
             <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
               <div className="about-wrapper__info">
                 <p className="about-wrapper__info-text">
-                  {about?.paragraphOne ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                  {about?.paragraphOne}
                 </p>
                 <p className="about-wrapper__info-text">
-                  {about?.paragraphTwo ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                  {about?.paragraphTwo}
                 </p>
                 <p className="about-wrapper__info-text">
-                  {about?.paragraphThree ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'}
+                  {about?.paragraphThree}
                 </p>
                 {about?.resume && (
                   <span className="d-flex mt-3">
@@ -69,7 +85,7 @@ const About = () => {
                       className="cta-btn cta-btn--resume"
                       href={about?.resume}
                     >
-                      {about?.btnText || 'Resume'}
+                      LinkedIn
                     </a>
                   </span>
                 )}
